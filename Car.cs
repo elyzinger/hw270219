1d7efb4abe3d863eef1ba39c6e20b546ad668b4a
elyzinger <45950112+elyzinger@users.noreply.github.com>
2019-02-27 15:53:45 +0200

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,75 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace Car
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        private int Codan { get; set; }
+        internal int NumberOfSeats { get; set; }
+        public Car(string model, string brand, int year, string color)
+        {
+            XmlSerializer filename = new XmlSerializer(typeof(Car));
+            Car desrializer;
+            using (Stream file = new FileStream(@"C:\Users\USER\source\repos\Car\Car\xmlcar.xml", FileMode.Open))
+            {
+                desrializer = filename.Deserialize(file) as Car;
+                
+            }
+            
+        }
+        public Car(string model, string brand, int year, string color, int codan, int numberofseats)
+        {
+            Model = model;
+            Brand = brand;
+            Year = year;
+            Color = color;
+            Codan = codan;
+            NumberOfSeats = numberofseats;
+        }
+          public int GetCodan()
+          {
+              return Codan;
+          }
+          public int GetNumberOfSeats()
+          {
+              return NumberOfSeats;
+          }
+        /*internal void ChangeNumberOfSeats()
+        {
+
+        }
+        public static void SerializeACar(filename, Car a)
+        {
+
+        }
+        public static SerialzerCarArray(filename, Car[] arraya)
+        {
+
+        }
+        public static DeserialzeACar(XmlSerializer filename)
+        {
+        
+             
+        }
+        public static DeserialzeCarArray(filename)
+        {
+            Car[];
+        }
+        public bool CarCompare(filename)
+        {
+            bool result;
+            result = false;
+            return result;           
+        }*/
+
+        public override string ToString()
+        {
+            return $"model: {Model} brand: {Brand} year: {Year} coler: {Color} ";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car a1 = new Car("yaris", "toyota", 2018, "red", 3232, 4);
+            Car[] cararray = new Car[]
+            {
+                new Car("yaris", "toyota", 2018, "red", 3232, 4),
+                new Car("yaris", "toyota", 2018, "red", 3232, 4),
+                new Car("yaris", "toyota", 2018, "red", 3232, 4),
+                new Car("yaris", "toyota", 2018, "red", 3232, 4),
+            };
+
+            XmlSerializer xmlcar = new XmlSerializer(typeof(Car[]));
+            // xml יצירה של אובייקט אחד 
+            /*using (Stream file = new FileStream(@"C:\Users\USER\source\repos\Car\Car\xmlcar.xml", FileMode.Create))
+            {
+                xmlcar.Serialize(file, a1);
+            }
+            // הורדה של xml 
+            Car adeserialzer;
+            using (Stream file = new FileStream(@"C:\Users\USER\source\repos\Car\Car\xmlcar.xml", FileMode.Open))
+            {
+               adeserialzer =  xmlcar.Deserialize(file) as Car;
+            }
+            Console.WriteLine(adeserialzer);
+            
+            // xml של מערך
+            using (Stream file = new FileStream(@"C:\Users\USER\source\repos\Car\Car\xmlcar.xml", FileMode.Create))
+            {
+                xmlcar.Serialize(file, cararray);
+            }
+            Car[] arrayout;
+            using (Stream file = new FileStream(@"C:\Users\USER\source\repos\Car\Car\xmlcar.xml", FileMode.Open))
+            {
+                arrayout = xmlcar.Deserialize(file) as Car[];
+            }
+            Console.WriteLine(arrayout[1]);*/
+            Console.WriteLine();
+        }
+    }
+
+}
